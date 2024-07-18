bfa1123a79f0e6527a15497bc7b43ef3b188fd71
emon <liming20110711@163.com>
2024-07-18 11:59:03 +0800

    update:20240718-03

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var env         = process.env.npm_config_qa ? 'qa' : process.env.npm_config_uat 
 console.log( '当前环境：\nprocess.env.npm_config_argv=', process.env.npm_config_argv, '\nprocess.argv=', process.argv )
 if (!process.env.npm_config_argv) {
   if (process.arch.length <= 2) {
-    throw new Error( '打得过你钱npm版本不支持process.env.npm_config_argv，请使用如下命令编译：\nnpm run build [qa|uat|prod]' )
+    throw new Error( '当前npm版本不支持process.env.npm_config_argv，请使用如下命令编译：\nnpm run build [qa|uat|prod]' )
   }
   env = process.argv.length > 2 ? process.argv[2] : env
 }
